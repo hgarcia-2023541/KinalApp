4399ea1bb3a7087707d238c0a8d046c7c1f69930
hgarcia-2023541 <hgarcia-2023541@kinal.edu.gt>
2026-04-16 23:39:10 -0600

    refactor: actualizar controlador REST de Usuario con prefijo api

--- src/main/java/com/herbertgarcia/kinalapp/controller/UsuarioController.java
+++ src/main/java/com/herbertgarcia/kinalapp/controller/UsuarioController.java
@@ -5,10 +5,11 @@ import com.herbertgarcia.kinalapp.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/api/usuarios")  // ← AGREGAR /api/
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
