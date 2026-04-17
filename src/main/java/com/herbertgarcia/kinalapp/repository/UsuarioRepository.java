a61b2f483fa2542164b93c342a2109e22394afb9
hgarcia-2023541 <hgarcia-2023541@kinal.edu.gt>
2026-04-17 00:03:16 -0600

    Revert "Merge pull request #38 from hgarcia-2023541/develop"
    This reverts commit b9149360cfc9409e1671a227d591d3b68cc96f6f, reversing
    changes made to 49e4bf39ced64f3789cf7c629f942312f57b053e.

--- src/main/java/com/herbertgarcia/kinalapp/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.herbertgarcia.kinalapp.repository;
-
-import com.herbertgarcia.kinalapp.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    List<Usuario> findByEstado(Long estado);
-}
